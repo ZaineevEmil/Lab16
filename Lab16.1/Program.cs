cf1a96d4448a1dc356e192b5de6049b36ea86501
2299112@gmail.com <2299112@gmail.com>
2021-12-04 16:27:57 +0500

    Добавьте файлы проекта.

---
+++ Lab16.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Lab16._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Необходимо разработать программу для получения информации о товаре из json-файла.
+            Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.*/
+
+            StreamReader jsonReadFileProducts = new StreamReader("Products.json");
+            string jsonStringProduct = jsonReadFileProducts.ReadToEnd();
+            Product[] product = new Product[5];
+            product = JsonSerializer.Deserialize<Product[]>(jsonStringProduct);
+            string dearestTitelProduct = "";
+            double dearestPriceProduct = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if(product[i].Price> dearestPriceProduct)
+                    {
+                    dearestTitelProduct = product[i].Title;
+                    dearestPriceProduct = product[i].Price;
+                }
+            }
+            Console.WriteLine(dearestTitelProduct);
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Lab16/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.IO;
+
+namespace Lab16
+{
+
+    // Моделирование объекта «Товар» в файл «Products.json».
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product[] product = new Product[5];
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    product[i] = new Product();
+                    Console.WriteLine("Введите данные {0} продукта", i + 1);
+                    Console.WriteLine("Введите код товара");
+                    product[i].Code = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Введите название товара");
+                    product[i].Title = Console.ReadLine();
+                    Console.WriteLine("Введите цену товара");
+                    product[i].Price = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("\n");
+                }
+            }
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic)
+            };
+            string jsonStringProducts = JsonSerializer.Serialize(product, options);
+            StreamWriter jsonWriteFileProducts = new StreamWriter("Products.json", false);
+            jsonWriteFileProducts.Write(jsonStringProducts);
+            jsonWriteFileProducts.Close();
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+            public int Code { get; set; }
+            public string Title { get; set; }
+            public double Price { get; set; }
+    }
+}
